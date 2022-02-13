40d0a985c2472da16f455b1398320eb0f82821fb
priyanshuborole <ppborole1762002@gmail.com>
2022-02-13 14:29:48 +0530

    fixed crash

--- app/src/main/java/com/priyanshu/mvvvmnewsapp/db/ArticleDatabase.kt
+++ app/src/main/java/com/priyanshu/mvvvmnewsapp/db/ArticleDatabase.kt
@@ -2,16 +2,16 @@ package com.priyanshu.mvvvmnewsapp.db
 
 import android.content.Context
 import androidx.room.*
+import androidx.room.migration.AutoMigrationSpec
 import com.priyanshu.mvvvmnewsapp.model.Article
 
 @Database(
-    version = 1,
-    entities = [Article::class],
+    version = 2,
+    entities = [Article::class]
 )
 
 @TypeConverters(Convertors::class)
 abstract class ArticleDatabase : RoomDatabase() {
-
     abstract fun getArticleDao(): ArticleDao
 
     companion object{
@@ -30,7 +30,7 @@ abstract class ArticleDatabase : RoomDatabase() {
              context.applicationContext,
              ArticleDatabase::class.java,
              "article-db.db"
-            ).build()
+            ).fallbackToDestructiveMigration().build()
 
 
     }
--- app/src/main/java/com/priyanshu/mvvvmnewsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/priyanshu/mvvvmnewsapp/ui/fragments/ArticleFragment.kt
@@ -34,7 +34,7 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         val article = args.article
         binding.webView.apply {
             webViewClient = WebViewClient()
-            loadUrl(article.url.toString())
+            loadUrl(article.url!!)
         }
 
         binding.fab.setOnClickListener {
